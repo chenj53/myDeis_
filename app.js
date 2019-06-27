8d81f665320898e9828d622b334c8db18f85dfe1
Jeffrey Chen <chenjeff53@gmail.com>
2019-06-27 11:09:01 -0400

    forum

--- app.js
+++ app.js
@@ -77,8 +77,6 @@ app.use((req,res,next) => {
   //console.dir(req.user)
   res.locals.status = 'none'
   if (req.user){
-       console.log(JSON.stringify(owners))
-       console.log(req.user.googleemail)
        if (owners.indexOf(req.user.googleemail)>-1){
          console.log("Owner has logged in")
          res.locals.status = 'owner'
@@ -91,7 +89,7 @@ app.use((req,res,next) => {
   console.log("res.locals = "+JSON.stringify(res.locals))
 
   next()
-}
+})
 
 
 
