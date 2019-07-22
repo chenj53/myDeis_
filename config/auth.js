6b29b981c9d3369a48f55359cb43d7d9ea34f17c
Tristan Tseng <tristantseng@brandeis.edu>
2019-07-22 11:23:43 -0400

    changeauth

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
         'clientID': '514085856294-n5q4nofnut2p4fnauhmbrpomoqb85rhn.apps.googleusercontent.com',
         'clientSecret'  : 'WlnIfp-2T7p2NHVmvpCWNKm6',
         // 'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
-        'callbackURL'   : 'http://morning-eyrie-60063.herokuapp.com/login/authorized'
+        'callbackURL'   : 'https://morning-eyrie-60063.herokuapp.com/login/authorized'
 
 
 
