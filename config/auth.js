1eca975ceabc6ad9e19f3c8eb88d9faadf8af42c
Tristan Tseng <tristantseng@brandeis.edu>
2019-07-23 11:42:19 -0400

    updated auth for heroku

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@ module.exports = {
     'googleAuth' : {
         'clientID': '514085856294-n5q4nofnut2p4fnauhmbrpomoqb85rhn.apps.googleusercontent.com',
         'clientSecret'  : 'WlnIfp-2T7p2NHVmvpCWNKm6',
-        'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
-        //'callbackURL'   : 'https://morning-eyrie-60063.herokuapp.com/login/authorized'
+        //'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
+        'callbackURL'   : 'https://morning-eyrie-60063.herokuapp.com/login/authorized'
 
 
 
